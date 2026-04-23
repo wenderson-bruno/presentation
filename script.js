489c5519d5c7f2eb1e0dfba2715608234bb742b5
icaru <creft294@gmail.com>
2026-04-23 10:04:25 -0300

    mais atualizações

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const skill = document.querySelector('.skills')
 const tag = document.querySelector('.skills-a')
 
+const section_de_contacts = document.querySelector('.contacts-section')
+const contact = document.querySelector('.contacts')
+
+
+/* skills */
 tag.addEventListener('mouseenter', (e) => {
     e.preventDefault()
     skill.classList.add('show')
@@ -8,4 +13,15 @@ tag.addEventListener('mouseenter', (e) => {
 tag.addEventListener('mouseleave', (e) => {
     e.preventDefault()
     skill.classList.remove('show')
+})
+
+/* contact */
+
+contact.addEventListener('mouseenter', (e) => {
+    e.preventDefault()
+    section_de_contacts.classList.add('visible')
+})
+contact.addEventListener('mouseleave', (e) => {
+    e.preventDefault()
+    section_de_contacts.classList.remove('visible')
 })
